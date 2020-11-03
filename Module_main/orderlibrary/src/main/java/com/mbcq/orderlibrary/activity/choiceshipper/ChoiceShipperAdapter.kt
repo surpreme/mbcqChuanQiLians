ee0028ee14598743dd986675ca64c6fdca8b58b4
LiZiYang <1740747328@.com>
2020-11-03 14:18:29 +0800

    受理开单重量计算器

---
+++ Module_main/orderlibrary/src/main/java/com/mbcq/orderlibrary/activity/choiceshipper/ChoiceShipperAdapter.kt
@@ -0,0 +1,41 @@
+package com.mbcq.orderlibrary.activity.choiceshipper
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.mbcq.baselibrary.view.BaseRecyclerAdapter
+import com.mbcq.baselibrary.view.SingleClick
+import com.mbcq.orderlibrary.R
+
+class ChoiceShipperAdapter(context: Context) : BaseRecyclerAdapter<ChoiceShipperBean>(context) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder = ItemViewHolder(inflater.inflate(R.layout.item_choice_shipper, parent, false))
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ItemViewHolder).basic_info_tv.text = "${mDatas[position].contactMan}    ${mDatas[position].contactMb}"
+        holder.address_info_tv.text = "地址：${mDatas[position].address}"
+        holder.company_info_tv.text = "公司：${mDatas[position].companyName}"
+        holder.telphone_info_tv.text = "固定电话：${mDatas[position].contactTel}"
+        holder.itemView.setOnClickListener(object : SingleClick() {
+            override fun onSingleClick(v: View) {
+                mClickInterface?.onItemClick(v, position, Gson().toJson(mDatas[position]))
+            }
+
+        })
+
+    }
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var basic_info_tv = itemView.findViewById<TextView>(R.id.basic_info_tv)
+        var address_info_tv = itemView.findViewById<TextView>(R.id.address_info_tv)
+        var company_info_tv = itemView.findViewById<TextView>(R.id.company_info_tv)
+        var telphone_info_tv = itemView.findViewById<TextView>(R.id.telphone_info_tv)
+        var modify_info_tv = itemView.findViewById<ImageView>(R.id.modify_info_tv)
+    }
+}
