3d7c4f53dec1c4d62d3171d220c471f59b82e97d
LiZiYang <1740747328@.com>
2021-03-31 18:06:34 +0800

    改单详情完善

---
+++ Module_main/orderlibrary/src/main/java/com/mbcq/orderlibrary/fragment/waybillinformation/WaybillInformationFixedTableBean.kt
@@ -0,0 +1,7 @@
+package com.mbcq.orderlibrary.fragment.waybillinformation
+
+data class WaybillInformationFixedTableBean(
+        var contentStr: String = "",
+        var beforeStr: String = "",
+        var endStr: String ="",
+        var isTitles:Boolean=false)
